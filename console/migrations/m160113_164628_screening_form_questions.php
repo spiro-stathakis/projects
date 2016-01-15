088cc734b5128466694038f08dc44f08217da413
Spiro Stathakis <spiro@macbook>
2016-01-15 09:50:32 +0000

    Adding some extra tables for screening

--- console/migrations/m160113_164628_screening_form_questions.php
+++ console/migrations/m160113_164628_screening_form_questions.php
@@ -23,10 +23,10 @@ class m160113_164628_screening_form_questions extends XMigration
                 $this->tableName, 
                 [
                     'id'=>$this->primaryKey(),
-                    'name'=>$this->string(255)->notNull(),
-                    'title'=>$this->string(4096),
-                    'description'=>$this->string(4096),
-                    'collection_id' => $this->integer()->notNull(),
+                    'title'=>$this->string(255)->notNull(),
+                    'screening_form_id'=>$this->integer()->notNull(),
+                    'input_type_id'=>$this->integer()->notNull(),
+                    'default_value'=>$this->integer()->notNull()->defaultValue(1),
                     'sort_order' => $this->integer()->notNull()->defaultValue(100), 
                     'status_id'=>$this->integer()->notNull()->defaultValue(2),
                     'created_at' => $this->integer()->notNull(),
@@ -37,12 +37,14 @@ class m160113_164628_screening_form_questions extends XMigration
                 $this->mysqlOptions 
             );
 
-        $this->addForeignKey('fk_screening_form_status_id' , $this->tableName,  'status_id' , 'ref_status' , 'id' , 'NO ACTION' , 'NO ACTION'); 
-        $this->addForeignKey('fk_screening_form_collection_id' , $this->tableName,  'collection_id' , 'collections' , 'id' , 'NO ACTION' , 'NO ACTION'); 
+        $this->addForeignKey('fk_screening_form_questions_screening_form_id' , $this->tableName,  'screening_form_id' , 'screening_forms' , 'id' , 'NO ACTION' , 'NO ACTION'); 
+        $this->addForeignKey('fk_screening_form_questions_input_type_id' , $this->tableName,  'input_type_id' , 'ref_input_type' , 'id' , 'NO ACTION' , 'NO ACTION'); 
+        $this->addForeignKey('fk_screening_form_questions_status_id' , $this->tableName,  'status_id' , 'ref_status' , 'id' , 'NO ACTION' , 'NO ACTION'); 
                    
-        $this->insert($this->tableName,['name'=>'MRI primary screening',
-                                    'description'=>'CUBRIC, Cardiff University - MRI screening form',
-                                    'collection_id'=>1,
+        $this->insert($this->tableName,['title'=>'Are you pregnant',
+                                    'screening_form_id'=>1,
+                                    'input_type_id'=>6,
+                                    'default_value'=>3, 
                                     'sort_order'=>100, 
                                     'status_id'=>2, 
                                     'created_at'=>time(),
@@ -56,9 +58,12 @@ class m160113_164628_screening_form_questions extends XMigration
   
     public function down()
     {
-        echo "m160113_164628_screening_form_questions cannot be reverted.\n";
-
-        return false;
+        $this->init();
+        $this->dropForeignKey('fk_screening_form_questions_screening_form_id',$this->tableName); 
+        $this->dropForeignKey('fk_screening_form_questions_input_type_id',$this->tableName); 
+        $this->dropForeignKey('fk_screening_form_questions_status_id',$this->tableName); 
+        $this->dropTable($this->tableName); 
+        return true;
     }
 
     /*
