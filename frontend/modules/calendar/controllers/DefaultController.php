906d25628f22c0f464d6306ae56e4c42198a0aca
Spiro Stathakis <spiro@macbook>
2016-05-22 09:40:35 +0100

    Missing class for mysqloptions

--- frontend/modules/calendar/controllers/DefaultController.php
+++ frontend/modules/calendar/controllers/DefaultController.php
@@ -29,7 +29,7 @@ class DefaultController extends XController
                         'rules' => [
                                     [
                                         'actions' => ['index'], 
-                                        'allow' => false,
+                                        'allow' => true,
                                         'roles' => ['@'],
                                      ], 
                                 ],
