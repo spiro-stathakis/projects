99cb7b7a4d32277cc7c5061c2ef2b7a932c193d2
Spiro Stathakis <spiro@macbook>
2016-08-09 11:42:35 +0100

    Working with mail component

--- common/components/UserComponent.php
+++ common/components/UserComponent.php
@@ -70,8 +70,10 @@ class UserComponent extends Object
 */ 
      Yii::$app->mail->compose(['html'=>'welcome-html',
                                'text'=>'welcome-text',  
-                               'user_name'=>'Spiro' 
-                              ])
+                              ], 
+                              ['user_name'=>'Spiro'
+                              ] 
+                              )
      ->setFrom('noreply@cardiff.ac.uk')
      ->setTo('spiro@cardiff.ac.uk')
      ->setSubject('Advanced email from Yii2-SwiftMailer')
