8c338533930c59addd201d11630870de2c74044c
Spiro Stathakis <spiro@macbook>
2016-08-09 11:51:58 +0100

    Working with mail component

--- common/mail/welcome-html.php
+++ common/mail/welcome-html.php
@@ -8,8 +8,11 @@ Dear <?=$user_name?>,
 
 You have been registered with the CUBRIC projects database.  You can access the system here: 
 <p>
-<?=Html::a(Url::to('/','https'),Url::to('/','https'));?> 
+<?=Html::a('https://projects.cubric.cf.ac.uk','https://projects.cubric.cf.ac.uk');?> 
 </p>
-
-
+If you experience any problems please contact support at cubricit@cf.ac.uk 
+<p>
+Thank you, 
+</p>
+The CUBRIC IT support team 
  
--- common/mail/welcome-text.php
+++ common/mail/welcome-text.php
@@ -4,12 +4,14 @@
 
 Dear <?=$user_name?>, 
 
-
+<br /> 
 
 You have been registered with the CUBRIC projects database.  You can access the system here: 
-
-<?=Url::to('/','https');?> 
-
-
-
- 
+<p>
+https://projects.cubric.cf.ac.uk
+</p>
+If you experience any problems please contact support at cubricit@cf.ac.uk 
+<p>
+Thank you, 
+</p>
+The CUBRIC IT support team 
